35f550ec3904d11ea0400800ad6c8f65dcbec93c
Yinming <ym@YinmingdeMacBook-Pro.local>
2018-05-04 10:01:30 +0800

    区域页面0.1v

--- webmoni/views.py
+++ webmoni/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from webmoni.models import MonitorData
+from webmoni.models import DomainName
+from webmoni.models import Project
 
 # Create your views here.
 def areas(request):
-    return render(request,'show_areas.html')
+    if request.method == 'GET':
+        projectList = {}
+        projectsObj = Project.objects.all().order_by('id')
+        for projectObj in projectsObj:
+            projectList[projectObj.name] = projectObj.domainname_set.all()
+
+        defaultDomainData = {}
+        defaultDomain = DomainName.objects.first()
+        defaultDomainData[defaultDomain.url] = defaultDomain.monitordata_set.all().values()
+        print(defaultDomainData)
+        return render(request,'show_areas.html',{'project_list':projectList,
+                                                 'defaultDomainData':defaultDomainData})
