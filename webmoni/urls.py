52a65a391b5161d4c8534732495b322da4b2640c
Yinming <ym@YinmingdeMacBook-Pro.local>
2018-05-16 13:30:49 +0800

    网站检测1.05

--- webmoni/urls.py
+++ webmoni/urls.py
@@ -4,14 +4,16 @@ from webmoni import api
 
 urlpatterns = [
     url(r'^$', views.areas),
-    url(r'^areas/', views.areas),
-    url(r'^areas-(\d*)/', views.areas),
-    url(r'^create/', views.create),
-    url(r'^delete/', views.delete),
-    url(r'^update_graph/', views.update_graph),
-    url(r'^update_domain/', views.update_domain),
-    url(r'^search/', views.search),
-    url(r'^api/domain_all/', api.domain_all),
-    url(r'^tables/', views.tables),
-    url(r'^tables/p/(\d+)', views.tables_project)
+    url(r'^areas/$', views.areas),
+    url(r'^areas-(\d*)/$', views.areas),
+    url(r'^create/$', views.create),
+    url(r'^delete/$', views.delete),
+    url(r'^update_graph/$', views.update_graph),
+    url(r'^update_domain/$', views.update_domain),
+    url(r'^search/$', views.search),
+    url(r'^api/domain_all/$', api.domain_all),
+    url(r'^tables/$', views.tables),
+    url(r'^tables/fault/$', views.tables_fault),
+    url(r'^tables/notcheck/$', views.tables_notcheck),
+    url(r'^tables/p/(\d+)/$', views.tables_project)
 ]
--- webmoni/views.py
+++ webmoni/views.py
@@ -135,9 +135,47 @@ def tables(request):
             'domainall':domainall,
             'Not_check_number':Not_check_number
         }
-
     return render(request,'domain_ table.html',{'data':data})
 
 
-def tables_project(request):
-    return render(request, 'domain_ table.html')
+def tables_project(request,project_id):
+    if request.method == 'GET':
+        project_all = Project.objects.all()
+        domainall = DomainName.objects.filter(project_name=project_id)
+        fault_number = DomainName.objects.filter(~Q(status_id=0) & Q(check_id=0)).count()
+        Not_check_number = DomainName.objects.filter(check_id=1).count()
+        data = {
+            'project_all':project_all,
+            'fault_number':fault_number,
+            'domainall':domainall,
+            'Not_check_number':Not_check_number
+        }
+    return render(request,'domain_ table.html',{'data':data})
+
+def tables_fault(request):
+    if request.method == 'GET':
+        project_all = Project.objects.all()
+        domainall = DomainName.objects.filter(~Q(status_id=0) & Q(check_id=0))
+        fault_number = DomainName.objects.filter(~Q(status_id=0) & Q(check_id=0)).count()
+        Not_check_number = DomainName.objects.filter(check_id=1).count()
+        data = {
+            'project_all':project_all,
+            'fault_number':fault_number,
+            'domainall':domainall,
+            'Not_check_number':Not_check_number
+        }
+    return render(request,'domain_ table.html',{'data':data})
+
+def tables_notcheck(request):
+    if request.method == 'GET':
+        project_all = Project.objects.all()
+        domainall = DomainName.objects.filter(check_id=1)
+        fault_number = DomainName.objects.filter(~Q(status_id=0) & Q(check_id=0)).count()
+        Not_check_number = DomainName.objects.filter(check_id=1).count()
+        data = {
+            'project_all':project_all,
+            'fault_number':fault_number,
+            'domainall':domainall,
+            'Not_check_number':Not_check_number
+        }
+    return render(request,'domain_ table.html',{'data':data})
