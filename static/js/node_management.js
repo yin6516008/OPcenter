f5be5ced1bd159e60954c0e6ed0968aadaa441c9
linpancolin@gmail.com <linpancolin@gmail.com>
2018-05-21 16:25:17 +0800

    节点管理页操作按钮

--- static/js/node_management.js
+++ static/js/node_management.js
@@ -14,7 +14,30 @@ $(function () {
     }
   })
 
+  // 删除按钮
+  $('[data-target="#delNode"]').click(function () {
+    var node = $(this).parent().parent().parent()
+    .parent().parent()
+    // 获取到对应的tr的节点号 --> 填充到隐藏域value
+    var nodeNum = node.find('td:nth-of-type(1)').text()
+    $('[type="hidden"]').val(nodeNum)
 
+    // 确认删除模态框中添加对应节点名
+    var nodePlace = node.find('td:nth-of-type(2)').text()
+    $('#delModalLabel').text('确认删除 "'+ nodePlace + '" 节点吗?')
+  })
 
+  // 编辑按钮
+  $('[data-target="#editNode"]').click(function () {
+    var node = $(this).parent().parent().parent()
+    .parent().parent()
+    var nodePlace = node.find('td:nth-of-type(2)').text()
+    var nodeIP = node.find('td:nth-of-type(3)').text()
+    var nodeDesc = node.find('td:nth-of-type(4)').text()
+    console.log(nodePlace, nodeIP)
+    $('#place').val(nodePlace)
+    $('#ipAddress').val(nodeIP)
+    $('#editDesc').val(nodeDesc)
+  })
 
 })
