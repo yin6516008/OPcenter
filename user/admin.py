dfbdf72071d212d8e8007d39f1ac26768b4adc87
Yinming <ym@YinmingdeMacBook-Pro.local>
2018-05-26 14:36:33 +0800

    修改user app表结构

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from user.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['id','user_name','online_status']
+
+
+admin.site.register(User,UserAdmin)
--- user/models.py
+++ user/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class user(models.Model):
+class User(models.Model):
     user_name = models.CharField(max_length=20,unique=True,)
     password = models.CharField(max_length=200,)
-    online_status = (
+    online_status_list = (
         ('Online','在线'),
         ('Offline','离线'),
         ('Exception','异常')
     )
-    online = models.CharField(max_length=2,choices=online_status,default='Offline')
+    online_status = models.CharField(max_length=20,choices=online_status_list,default='Offline')
+    def __str__(self):
+        return self.user_name
