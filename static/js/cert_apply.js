b270a16ac641ea3083c3017e90c277411a9680bf
linpancolin@gmail.com <linpancolin@gmail.com>
2018-05-28 15:21:58 +0800

    证书申请完善

--- static/js/cert_apply.js
+++ static/js/cert_apply.js
@@ -5,14 +5,26 @@ $(function () {
   $('#side-menu>li:nth-of-type(5)>ul').addClass('in')
 
   $('#getTXT').click(function () {
+      $('#wave').show() // 点击按钮 显示动画
       var domain = $('.input-lg').val()
-      console.log(domain)
       $.ajax({
           url:'/cert/apply/postdomain/',
           data:{'domain':domain},
           type:'post',
           success:function (result) {
-              $('#TXT_info').html(result)
+              $('#wave').hide()
+              JSON.parse(result)
+              if(result.status == 'OK') {
+                $('#TXT_info').show()
+                var host = result.host
+                var TXT = result.TXT
+                $('#hostname').text(host)
+                $('#TXTval').text(TXT)
+              }else {
+                $('#err').show()
+                var errInfo = result.data
+                $('#err>pre').text(errInfo)
+              }
           }
       })
   })
