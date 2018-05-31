ef05d5f0f7070c3775c73e8354acf2fe5ab7bbaa
unknown <492960429@qq.com>
2018-05-31 09:34:56 +0800

    证书列表样式、

--- static/js/cert_list.js
+++ static/js/cert_list.js
@@ -25,19 +25,45 @@ $(function () {
                 var files = JSON.parse(msg)
                 $('#file_table').html('')
                 for (i in files){
-                   var file = "<div class='file-box'>\
-                        <div class='file'>\
-                        <a href='/cert/download/" + domain + "/" + files[i] +"'>\
-                        <span class='corner'></span>\
-                        <div class='icon'>\
-                        <i class='fa fa-file'></i>\
-                        </div>\
-                        <div class='file-name'>" + files[i] + "\
-                        <br/>\
-                    </div>\
-                    </a>\
-                </div>";
-                $('#file_table').append(file)
+                   // var file = "<div class='file-box'>\
+                   //                <div class='file'>\
+                   //                   <a href='/cert/download/" + domain + "/" + files[i] +"'>\
+                   //                      <span class='corner'></span>\
+                   //                      <div class='icon'>\
+                   //                          <i class='fa fa-file'></i>\
+                   //                      </div>\
+                   //                      <div class='file-name'>" + files[i] + "\
+                   //                          <br/>\
+                   //                      </div>\
+                   //                   </a>\
+                   //               </div>\
+                   //            </div>";
+
+                    var file = `<div class='file-box'>
+                                  <div class='file'>
+                                     <a href='/cert/download/${domain}/${files[i]}'>
+                                        <span class='corner'></span>
+                                        <div class='icon'>
+                                            <i class='fa fa-file'></i>
+                                        </div>
+                                        <div class='file-name'>${files[i]}
+                                            <br/>
+                                        </div>
+                                     </a>
+                                 </div>`
+
+
+                   var postname = files[i].split('.')[files[i].split('.').length - 1]
+                    var file_table = $('#file_table')
+                    var file_fa = $(file).find('.fa')
+                    if(postname == 'conf') {
+                        file_table.append(file_fa.removeClass('fa-file').addClass('fa-cog').parents('.file-box')[0])
+                    }else if(postname == 'key') {
+                        file_table.append(file_fa.removeClass('fa-file').addClass('fa-key').parents('.file-box')[0])
+                    }else if(postname == 'cer') {
+                        file_table.append(file_fa.removeClass('fa-file fa').addClass('iconfont icon-zhengshu-copy').parents('.file-box')[0])
+
+                    }
 
                 }
             }
